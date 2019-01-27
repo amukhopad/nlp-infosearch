129708a08dc88174af88a69c256d9ae16fe6c211
Oleksandr Mukhopad <omukhopad@mapr.com>
2019-01-27 16:49:08 +0200

    Implement file indexing to json

--- util/file.py
+++ util/file.py
@@ -1,4 +1,19 @@
 from typing import List
+import os
+
+import util.pdf
+import util.doc
+
+
+def handle_formats(filename: str) -> str:
+    file_type = extension(filename)
+
+    if file_type == 'pdf':
+        filename = util.pdf.to_text(filename)
+    elif file_type == 'docx':
+        filename = util.doc.to_text(filename)
+
+    return filename
 
 
 def extension(filename: str) -> str:
@@ -10,7 +25,7 @@ def extension(filename: str) -> str:
 
 
 def txt_name(filename: str) -> str:
-    return split_extension(filename)[0] + '.txt'
+    return split_extension(filename)[0] + '_temp.txt'
 
 
 def split_extension(filename: str) -> List[str]:
